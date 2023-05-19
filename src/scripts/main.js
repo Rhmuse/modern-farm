427c7bbe09efa93bc8077aa5a46830179b4f0209
Rhett Muse <rhett.muse123@gmail.com>
2023-05-19 13:11:23 -0500

    reorganize main.js and remove comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,12 @@
 console.log("Welcome to the main module")
 import { createPlan } from "./plan.js";
+import { tractor } from './tractor.js';
+import { harvestPlants } from './harvester.js';
+import { catalog } from './catalog.js';
 
 const yearlyPlan = createPlan();
-
-// console.log(yearlyPlan);
-
-// import { createAsparagus } from "./seeds/asparagus.js"
-// import { createCorn } from "./seeds/corn.js";
-
-// const asparagusSeed = createAsparagus()
-// console.log(asparagusSeed)
-
-// import { addPlant, usePlants } from './field.js';
-
-// addPlant(createAsparagus());
-// addPlant(createCorn());
-
-// console.log(usePlants());
-
-import { tractor } from './tractor.js';
-tractor(yearlyPlan); 
+const sownfield = tractor(yearlyPlan);
+const harvest = harvestPlants(sownfield);
+const inventory = catalog(harvest);
+const parentHTMLElement = document.querySelector("main");
+parentHTMLElement.innerHTML = inventory.join("\n");
