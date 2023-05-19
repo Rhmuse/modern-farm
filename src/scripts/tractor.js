fda6a856795c43c0c12cc63c7fb6cbe15a7b6017
Rhett Muse <rhett.muse123@gmail.com>
2023-05-19 10:42:01 -0500

    make tractor function return seed obj

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,15 +1,41 @@
-import { addPlant, usePlants } from "./field.js"
+import { addPlant, usePlants } from "./field.js";
+import { createAsparagus } from './seeds/asparagus.js';
+import { createCorn } from './seeds/corn.js';
+import { createPotato } from './seeds/potato.js';
+import { createSoybean } from './seeds/soybean.js';
+import { createSunflower } from './seeds/sunflower.js';
+import { createWheat } from './seeds/wheat.js';
 
 export const tractor = (plan) => {
     for (const row of plan) {
         for (const seed of row) {
-            if (typeof seed === Array) {
-                addPlant(seed[0])
-            } else {
-                addPlant(seed);
+            // if (typeof seed === Array) {
+            //     addPlant(seed[0])
+            // } else {
+            //     addPlant(seed);
+            // }
+            if (seed === "Asparagus") {
+                const asparagus = createAsparagus();
+                addPlant(asparagus);
+            } else if (seed === "Corn") {
+                const corn = createCorn();
+                addPlant(corn[0]);
+            } else if (seed === "Potato") {
+                const potato = createPotato();
+                addPlant(potato);
+            } else if (seed === "Soybean") {
+                const soybean = createSoybean();
+                addPlant(soybean);
+            } else if (seed === "Sunflower") {
+                const sunflower = createSunflower();
+                addPlant(sunflower);
+            } else if (seed === "Wheat") {
+                const wheat = createWheat();
+                addPlant(wheat);
             }
         }
     }
     let sownField = usePlants();
     console.log(sownField);
+    return sownField;
 }
