8e29a16a5df103bb040b04d2b530370a542f955e
Rhett Muse <rhett.muse123@gmail.com>
2023-05-19 10:13:51 -0500

    add temp testing to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,14 @@ const yearlyPlan = createPlan();
 console.log(yearlyPlan);
 
 import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js";
 
 const asparagusSeed = createAsparagus()
 console.log(asparagusSeed)
+
+import { addPlant, usePlants } from './field.js';
+
+addPlant(createAsparagus());
+addPlant(createCorn());
+
+console.log(usePlants());
