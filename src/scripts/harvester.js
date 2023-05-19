88bb0cd690a469c41607a4b8a8db60b768e2cdbf
Rhett Muse <rhett.muse123@gmail.com>
2023-05-19 13:09:58 -0500

    define harvestPlants

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,3 +1,9 @@
-export const harvestPlants = (plants) => {
-
-}
+export const harvestPlants = (sownField) => {
+    let harvest = [];
+    for (const plant of sownField) {
+        for (let i = 0; i < plant.output; i++) {
+            harvest.push(plant);
+        }
+    }
+    return harvest;
+};   
