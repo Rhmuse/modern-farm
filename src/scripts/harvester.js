98cd4d9ff9a524f13b62d08be21b2b6e66ea1854
Rhett Muse <rhett.muse123@gmail.com>
2023-05-19 23:04:58 -0500

    adjust output based on corn special condition

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,8 +1,14 @@
 export const harvestPlants = (sownField) => {
     let harvest = [];
+    let output = 0;
     for (const plant of sownField) {
-        for (let i = 0; i < plant.output; i++) {
-            harvest.push(plant);
+        if (plant.type === "Corn") {
+            output = plant.output / 2;
+        } else {
+            output = plant.output;
+        }
+        for (let i = 0; i < output; i++) {
+            harvest.push(plant)
         }
     }
     return harvest;
